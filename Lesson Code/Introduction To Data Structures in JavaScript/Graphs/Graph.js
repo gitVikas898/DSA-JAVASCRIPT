6450b250d976dbc1515cd8dd51c291db8c58777f
Vikas <vikas.kumar.singh1198@gmail.com>
2024-12-26 12:06:51 +0530

    Finished Basic Graphs , Implementing Graph Class, addVertex remove Vertex , addEdge , removeEdge methods

--- Lesson Code/Introduction To Data Structures in JavaScript/Graphs/Graph.js
+++ Lesson Code/Introduction To Data Structures in JavaScript/Graphs/Graph.js
@@ -1,22 +1,36 @@
 class Graph {
-    constructor(){
-        this.adjacencyList = {};
-    }
+  constructor() {
+    this.adjacencyList = {};
+  }
 
-    addVertex(name){
-        if(!this.adjacencyList[name]){
-            this.adjacencyList[name] = [];
-            return true;
-        }
-        return false;
+  addVertex(name) {
+    if (!this.adjacencyList[name]) {
+      this.adjacencyList[name] = [];
+      return true;
     }
-    addEdge(v1,v2){
-      
-            this.adjacencyList[v1].push(v2);
-            this.adjacencyList[v2].push(v1); 
-        
-            I
+    return false;
+  }
+  addEdge(v1, v2) {
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+  }
+
+  removeEdge(vertex1,vertex2){
+    
+    this.adjacencyList[vertex1] =this.adjacencyList[vertex1].filter(v=>v!=vertex2);
+
+    this.adjacencyList[vertex2] =this.adjacencyList[vertex2].filter(v=>v!=vertex1);
+
+  }
+
+  removeVertex(vertex){
+
+    while(this.adjacencyList[vertex].length){
+        let adjacentVertex = this.adjacencyList[vertex].pop();
+        this.removeEdge(vertex,adjacentVertex);
     }
+    delete this.adjacencyList[vertex];
+  }
 }
 
 let graph = new Graph();
@@ -25,9 +39,14 @@ graph.addVertex("Tokyo");
 graph.addVertex("Singapore");
 graph.addVertex("Shanghai");
 
+graph.addEdge("Tokyo", "Singapore");
+graph.addEdge("Shanghai", "Tokyo");
+graph.addEdge("Shanghai", "Singapore");
 
-graph.addEdge("Tokyo","Singapore");
-graph.addEdge("Shanghai","Tokyo");
+console.log(graph.adjacencyList);
 
-console.log(graph.adjacencyList)
+graph.removeVertex("Singapore");
+
+console.log(graph.adjacencyList);
+
 
